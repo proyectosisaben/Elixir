f0f9a09eaedb67e66f919028d9b463cdf986ac12
proyectosisaben <proyectosisaben@gmail.com>
2025-12-15 03:57:24 -0300

    Fix: Corregir referencias a todocarro en wsgi.py y asgi.py

--- Backend/elixir_db/asgi.py
+++ Backend/elixir_db/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for todocarro project.
+ASGI config for elixir_db project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todocarro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elixir_db.settings')
 
 application = get_asgi_application()
--- Backend/elixir_db/wsgi.py
+++ Backend/elixir_db/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for todocarro project.
+WSGI config for elixir_db project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todocarro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elixir_db.settings')
 
 application = get_wsgi_application()
